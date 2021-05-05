266cc99c1f0e16f237b26ccb98b4284892f2c0f3
Iric <iricbing@gmail.com>
2021-05-05 16:13:25 +0800

    增加templateMessageService导出

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { OfficialResponseCode } from './constants/official.constant';
 
 export * from './services/auth.service';
 export * from './services/message.service';
+export * from './services/template-message.service';
