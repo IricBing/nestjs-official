04756365c775ab647442f00e712e163f338088d9
Iric <iricbing@gmail.com>
2021-06-07 17:15:07 +0800

    增加git pushlish sh脚本

---
+++ git-publish.sh
@@ -0,0 +1,8 @@
+git push --all &&
+  git push --all github &&
+  git push --all lantsang-github &&
+  git push --all lantsang-gitee &&
+  git push --tags &&
+  git push --tags github &&
+  git push --tags lantsang-github &&
+  git push --tags lantsang-gitee
